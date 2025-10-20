8ae7dec9625445a71125c9d5b194d8fc66898e02
Kinderek <Kinderek@DESKTOP-B8SV7SU>
2025-10-20 23:43:57 +0200

    Dodaj pliki projektów.

---
+++ Element.h
@@ -0,0 +1,11 @@
+#pragma once
+using namespace std;
+
+struct Element {
+    int wartosc;
+    Element* nast;
+    Element* poprz;
+
+
+    Element(int w) : wartosc(w), nast(nullptr), poprz(nullptr) {}
+};
